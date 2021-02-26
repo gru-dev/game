11a7e5e92b4eb5530f08166839b0d74b93682ee6
WranDm <TitusPahn2019@u.northwestern.edu>
2021-02-26 00:02:21 -0600

    walljumping proto & basic camera mvmt

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,13 @@ public class PlayerController : MonoBehaviour
     private BoxCollider2D _col;
     private LayerMask _walls;
     private LayerMask _floors;
-    
+
     public float Speed;
     public float JumpPower;
     public bool IsGrounded;
     public bool TouchWallToLeft;
     public bool TouchWallToRight;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,26 +27,59 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        var side_movement = Input.GetAxis("Horizontal");
-        var jump = Input.GetAxis("Jump");
-        
+        var side_movement = Input.GetAxisRaw("Horizontal");
+        var jump = Input.GetAxisRaw("Jump");
+
         GroundedRay();
         LeftWallRay();
         RightWallRay();
-        
+
         // Horizontal movement
         Vector2 apply_force = new Vector2(side_movement * Speed, 0) - new Vector2(_rb.velocity.x, 0);
         _rb.AddForce(apply_force, ForceMode2D.Impulse);
-        
+
         // Jump
-        if (IsGrounded & jump > 0)
+        // Edit: Moved to a separate function.
+        if (jump > 0)
+        {
+            PlayerJump(jump);
+        }
+
+    }
+
+    private void PlayerJump(float j)
+    {
+        Debug.Log(j);
+        //Simple jumping from the ground up
+        if (IsGrounded)
         {
             Debug.Log("applying force");
             Vector2 jump_force = new Vector2(0, JumpPower) - new Vector2(_rb.velocity.x, 0);
             _rb.AddForce(jump_force, ForceMode2D.Impulse);
         }
+        //Wall jumping off of a wall to the left
+        else if (TouchWallToLeft)
+        {
+            Debug.Log("wall jump off left");
+            Vector2 walljump_left = new Vector2(JumpPower * 2, JumpPower * 4 / 3) - new Vector2(_rb.velocity.x, 0);
+            _rb.AddForce(walljump_left, ForceMode2D.Impulse);
+            Debug.Log(walljump_left);
+        }
+        //Wall jumping off of a wall to the left
+        else if (TouchWallToRight)
+        {
+            Debug.Log("wall jump off right");
+            Vector2 walljump_right = new Vector2(-JumpPower * 2, JumpPower * 4 / 3) - new Vector2(_rb.velocity.x, 0);
+            _rb.AddForce(walljump_right, ForceMode2D.Impulse);
+            Debug.Log(walljump_right);
+        }
+        //Jump pressed midair
+        else
+        {
+            Debug.Log("no jump");
+        }
     }
-    
+
     private void GroundedRay()
     {
         // Get leftmost and rightmost positions on player collider
